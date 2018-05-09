3ac22c3b84c3748d47b68158940f6c84cd0330d0
Paul <pcherian10@gmail.com>
2018-05-09 14:01:36 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist
+attr_accessor :name
+attr_reader :songs
+
+  def initialize (name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song (song)
+    @songs << song
+  end
+
+  def songs
+    @songs
+  end
+
+end
