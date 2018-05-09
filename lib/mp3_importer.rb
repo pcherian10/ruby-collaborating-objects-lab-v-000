26e4a73c7062cfcad25a27ac9cb1d28936a3a903
Paul <pcherian10@gmail.com>
2018-05-09 14:57:22 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,13 +1,17 @@
+require".pry"
+
 class MP3Importer
 
   attr_accessor :path, :files
 
   def initialize(music_path)
     @path = music_path
+
   end
 
   def files
     @files = Dir.entries(path).select {|file| file if file.include?(".mp3")}
+    binding.pry
   end
 
   def import
